c774c1e6587b2fe6265e7e36141425b0d5cd1f19
Faye Chen <fchen328@gatech.edu>
2024-05-03 23:40:24 -0700

    Top and Bottom

--- dogfoodcup.py
+++ dogfoodcup.py
@@ -2,23 +2,36 @@ from solid import *
 from solid.utils import *
 from solid.screw_thread import *
 
-volumn = 354
-h = 6
-r = sqrt(volumn/(h*3.14))
 thickness = 0.3
-t=0.1
-threads = thread([[0.15, -0.5+t],[0.15, 0-t], [-0.15, 0+t],[-0.15, -0.5-t] ],r,1,h+1)
+volume = 1*236
+h = 6
+turnvolume = volume/4
 
+
+r = sqrt(volume/(h*3.14))+thickness/2
+pitch = turnvolume/(3.14*r*r)
+
+t=0.1
+
+threads = thread([[0.15, -0.5+t],[0.15, 0-t], [-0.15, 0+t],[-0.15, -0.5-t] ],r,pitch,h+1)
+
+
+indicator=translate([0,(r+thickness*1.5)/2,0])(cube([thickness,(r+thickness*1.5),thickness],center=True))
+
+indicator2=cube([thickness,(r+thickness*1.5)*2,thickness],center=True)
 
 bottom = difference()(
-    cylinder(r=r+thickness,h=h),
+    union()(cylinder(r=r+thickness,h=h),up(h-thickness/2)(indicator2)),
     up(thickness)(union()(threads,cylinder(r=r,h=h))))
 
 
-top = difference()(cylinder(r=r+thickness-0.01,h=h+thickness),union()(bottom,cylinder(r=r-thickness,h=h*2)))
+top = difference()(
+    union()(cylinder(r=r+thickness-0.01,h=h+thickness),up(h+thickness/2)(indicator)),
+    union()(bottom,cylinder(r=r-thickness,h=h*2)))
 
 
 print(r)
-scad_render_to_file(top, 'dogfoodcup.scad', file_header = '$fn=180;')
+scad_render_to_file(top, 'dogfoodcuptop.scad', file_header = '$fn=180;')
+scad_render_to_file(bottom, 'dogfoodcupbottom.scad', file_header = '$fn=180;')
 
 
